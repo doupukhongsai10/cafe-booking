6a1b90777c53f00d306927e594706bc82f5ce18a
Doupu Khongsai <doupukhongsai10@gmail.com>
2026-07-17 12:30:20 +0530

    fix: inline tailwind configuration inside vite.config.js to ensure dev-mode compiles correctly

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   plugins: {
     tailwindcss: { config: './client/tailwind.config.js' },
     autoprefixer: {},
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./client/index.html",
     "./client/src/**/*.{js,ts,jsx,tsx}",
   ],
